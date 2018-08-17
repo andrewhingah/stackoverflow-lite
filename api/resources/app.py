65f5772ce29631ff8cffffed14a9afeb81888e92
andrewhingah <andrewhinga5@gmail.com>
2018-08-17 18:59:26 +0300

    [Feature 159775891] Implement post answer

--- api/resources/app.py
+++ api/resources/app.py
@@ -1,25 +1,26 @@
-# from flask_api import FlaskAPI
+"""Endpoints are defined here"""
+
 from flask import Flask, jsonify, abort, make_response, request
 
-# from instance.config import app_config
-
-# def create_app(config_name):
 
 app = Flask(__name__)
 
-
+#questions list
 questions = [
     {
         'id': 1,
-        'question': 'What is API?'
+        'question': 'What is API?',
+        'answer':[]
     },
     {
         'id': 2,
-        'question': 'How to write a simple CRUD API?'
+        'question': 'How to write a simple CRUD API?',
+        'answer':[]
     },
     {
         'id': 3,
-        'question': 'Best framework for frontend development'
+        'question': 'Best framework for frontend development',
+        'answer':[]
     },
 ]
 
@@ -33,18 +34,12 @@ def _record_exists(question):
 def bad_request(error):
     return make_response(jsonify({'error': "bad request"}), 400)
 
-
+"""get all questions"""
 @app.route('/api/v1/questions', methods=['GET'])
 def get_questions():
     return jsonify({'questions': questions})
 
-@app.route('/api/v1/questions/<int:id>', methods=['GET'])
-def get_question(id):
-    question = _get_question(id)
-    if not question:
-        abort(404)
-    return jsonify({'questions': question})
-
+"""post a question"""
 @app.route('/api/v1/questions', methods=['POST'])
 def post_question():
     if not request.json or 'question' not in request.json:
@@ -53,10 +48,19 @@ def post_question():
     question = request.json.get('question')
     if _record_exists(question):
         abort(400)
-    question = {"id": question_id, "question": question}
+    question = {"id": question_id, "question": question, "answer":[]}
     questions.append(question)
     return jsonify({'question': question}), 201
 
+"""get a single question by id"""
+@app.route('/api/v1/questions/<int:id>', methods=['GET'])
+def get_question(id):
+    question = _get_question(id)
+    if not question:
+        abort(404)
+    return jsonify({'questions': question})
+
+"""delete a single question by id"""
 @app.route('/api/v1/questions/<int:id>', methods=['DELETE'])
 def delete_question(id):
     question = _get_question(id)
@@ -64,3 +68,13 @@ def delete_question(id):
         abort(404)
     questions.remove(question[0])
     return jsonify({}), 204
+
+"""post an answer to a question by id"""  
+@app.route('/api/v1/questions/answer/<int:id>', methods=['POST'])
+def post_answer(id):
+    question = _get_question(id)
+    
+    answer = request.json.get('answer')
+    question[0]["answer"].append(answer)
+
+    return jsonify({'questions': question}), 201
