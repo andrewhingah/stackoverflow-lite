1d567eb0378df0b056f4891a0ee5314f7e441cde
andrewhingah <andrewhinga5@gmail.com>
2018-08-17 10:55:27 +0300

    [Chore 159775514] Define create_app function

--- api/resources/questions.py
+++ api/resources/questions.py
@@ -1,28 +1,48 @@
+from flask_api import FlaskAPI
 from flask import Flask, jsonify, abort, make_response, request
 
-app = Flask(__name__)
+from instance.config import app_config
 
-questions = [
-    {
-        'id': 1,
-        'question': 'What is API?'
-    },
-    {
-        'id': 2,
-        'question': 'How to write a simple CRUD API?'
-    },
-    {
-        'id': 3,
-        'question': 'Best framework for frontend development'
-    },
-]
+def create_app(config_name):
+
+    app = Flask(__name__)
 
 
-@app.errorhandler(400)
-def bad_request(error):
-    return make_response(jsonify({'error': "bad request"}), 400)
+    questions = [
+        {
+            'id': 1,
+            'question': 'What is API?'
+        },
+        {
+            'id': 2,
+            'question': 'How to write a simple CRUD API?'
+        },
+        {
+            'id': 3,
+            'question': 'Best framework for frontend development'
+        },
+    ]
+
+    def _get_question(id):
+        return [question for question in questions if question['id'] == id]
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return make_response(jsonify({'error': "bad request"}), 400)
 
 
-@app.route('/api/v1/questions', methods=['GET'])
-def get_items():
-    return jsonify({'questions': questions})
+    @app.route('/api/v1/questions', methods=['GET'])
+    def get_questions():
+        return jsonify({'questions': questions})
+
+    @app.route('/api/v1/questions/<int:id>', methods=['GET'])
+    def get_question(id):
+        question = _get_question(id)
+        if not question:
+            abort(404)
+        return jsonify({'questions': question})
+
+    return app
+
+# if __name__ == '__main__':
+#     app.run(debug = True)
